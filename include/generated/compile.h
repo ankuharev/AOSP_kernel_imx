6d0616a34ea36e9e30a516b6fe10b65d8989c77e
Andrei Kukharev <an.kuharev@gmail.com>
2020-03-11 12:30:10 +0000

    Test kernel -DDEBUG

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Wed Mar 11 12:10:05 GMT 2020"
+#define UTS_VERSION "#5 SMP PREEMPT Wed Mar 11 12:29:13 GMT 2020"
 #define LINUX_COMPILE_BY "osboxes"
 #define LINUX_COMPILE_HOST "osboxes"
 #define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
